a99b3f0170486a95d0208c4582175e43221f9fe5
Rikard Skotnicki <rs22ef@student.lnu.se>
2012-02-13 19:31:40 +0100

    forbattringar samt dokumentation i naturaldocs

---
+++ fyrairad/addpiece.js
@@ -0,0 +1,8 @@
+/*
+   Function: addPieceTest
+   adds another piece
+ */
+
+function addPieceTest(row) {
+	alert('Användaren lägger ny pjäs i rad ' + row);
+}
---
+++ fyrairad/newgame.js
@@ -0,0 +1,8 @@
+/*
+  Function: newGameTest
+  this is another comment on the new game function
+*/
+
+function newGameTest() {
+	alert('nytt spel');
+}
